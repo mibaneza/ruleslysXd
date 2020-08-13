d6536d42db3d642f52e37a4d57636a8b95c668ca
mibaneza <49458798+mibaneza@users.noreply.github.com>
2020-08-13 23:03:00 +0000

    add
    ga

---
+++ src/test/java/com/intocables/rulesly/service/UserPunishServiceTest.java
@@ -0,0 +1,52 @@
+package com.intocables.rulesly.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigInteger;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import com.intocables.rulesly.repository.IUserPunishRepository;
+import com.intocables.rulesly.service.exception.RuleslyException;
+
+@SpringBootTest
+class UserPunishServiceTest {
+	
+	@Mock
+	IUserPunishRepository userPunhisRepository;
+	
+	@InjectMocks
+	UserPunishService userPunishService;
+	
+	BigInteger integar;
+	
+	@BeforeEach
+	public void init() throws RuleslyException{
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void findSQLAverageTest()throws RuleslyException{
+		 integar = new BigInteger("1800");
+		Mockito.when(userPunhisRepository.findSQLByQuantity()).thenReturn(integar);
+		userPunishService.findSQLAverage();
+	}
+	@Test
+	public void findSQLAverageTestError()throws RuleslyException{
+		Mockito.when(userPunhisRepository.findSQLByQuantity()).thenReturn(null);
+		
+		Assertions.assertThrows(RuleslyException.class, () -> {
+			integar = new BigInteger("");
+		    });
+		 
+		userPunishService.findSQLAverage();
+	}
+}
