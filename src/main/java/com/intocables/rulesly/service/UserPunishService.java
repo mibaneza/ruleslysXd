aab6f05408c5407767a3b4b3b397c297c6b7378d
mibaneza <mibaneza@ucvvirtual.edu.pe>
2020-08-16 01:25:55 -0500

    v1.013

--- src/main/java/com/intocables/rulesly/service/UserPunishService.java
+++ src/main/java/com/intocables/rulesly/service/UserPunishService.java
@@ -3,8 +3,6 @@ package com.intocables.rulesly.service;
 import java.math.BigInteger;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
